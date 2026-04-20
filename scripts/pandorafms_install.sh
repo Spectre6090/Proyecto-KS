82fb7d381f2332f5d039589aad37fd4fb2427485
Spectre6090 <kevmurgad@gmail.com>
2026-04-20 10:41:25 +0200

    pandorafms_install.sh

--- scripts/pandorafms_install.sh
+++ scripts/pandorafms_install.sh
@@ -6,16 +6,17 @@ env TZ='Europe/Madrid' \
  DBHOST='127.0.0.1' \
  DBNAME='pandora' \
  DBUSER='pandora' \
- DBPASS='wjI4XT9WahZFfioyioYsqukx' \
+ DBPASS='wjI4XT9WahZFfioyioYsqukx_' \
  DBPORT='3306' \
- DBROOTPASS='wjI4XT9WahZFfioyioYsqukx' \
+ DBROOTPASS='wjI4XT9WahZFfioyioYsqukx_' \
  MYVER=80 \
- PHPVER=8 \
+ PHPVER=8.2 \
  SKIP_PRECHECK=0 \
  SKIP_DATABASE_INSTALL=0 \
  SKIP_KERNEL_OPTIMIZATIONS=0 \
  PANDORA_LTS=1 \
  PANDORA_ENT_FREE=1 \
  bash -c "$(curl -SsL https://pfms.me/deployenterprise)"
+
 
 
